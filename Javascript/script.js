6b69dc06b2b00da2227cb29eddbcfca5e634ae70
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-07 04:27:51 +0200

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,29 @@
+let firstName = 'Elpida';
+console.log(firstName);
+
+let number = 4+4;
+console.log(number);
+
+let string = "4"+"4";
+console.log(string);
+
+let a = 9+7;
+console.log(a);
+
+let b = 57-9;
+console.log(b);
+
+let c = 4*4;
+console.log(c);
+
+let d = 10/2;
+console.log(d);
+
+let e = 10%100;
+console.log(e);
+
+var x = 48;
+console.log(typeof x);
+
+var x = 'fortyeight';
+console.log(typeof x);
